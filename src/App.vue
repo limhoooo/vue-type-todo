403b463fbea0f9a82dcbb6dd2f8771db41bbf85d
lim ho <dlagh123@gmail.com>
2021-05-26 23:43:00 +0900

    push

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Vue Todo with Typescript</h1>
+    <todo-input :item="todoText"> </todo-input>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TodoInput from "./components/TodoInput.vue";
+
+export default Vue.extend({
+  components: { TodoInput },
+  data() {
+    return {
+      todoText: "",
+    };
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" :value="item" />
+    <button @abort="addTodo">add</button>
+  </div>
+</template>
+
+
+
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["item"],
+  methods: {
+    addTodo() {
+      console.log("add");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
